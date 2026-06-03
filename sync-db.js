7216c3be5351460adb9646b8f34e0c78da0e75b4
Emi-legui <emilegui76@gmail.com>
2026-06-03 03:30:03 -0300

    Realizando pruebas

---
+++ sync-db.js
@@ -0,0 +1,14 @@
+import sequelize from './config/database.js';
+import User from './models/user.js';
+
+async function sincronizarBaseDeDatos() {
+    try {
+        await sequelize.sync({ force: true });
+        console.log('Tablas creadas con exito en la base de datos.');
+    } catch (error) {
+        console.error('Error al sincronizar:', error.message);
+    } finally {
+        process.exit();
+    }
+}
+sincronizarBaseDeDatos();
