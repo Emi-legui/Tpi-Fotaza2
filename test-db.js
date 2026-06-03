4639ae30cd9554df70fb5674cbbc070d324cb8d4
Emi-legui <emilegui76@gmail.com>
2026-06-03 03:15:36 -0300

    se probo y se corrigieron errores

--- test-db.js
+++ test-db.js
@@ -1,8 +1,8 @@
-import { Sequelize } from './config/database.js';
+import  sequelize  from './config/database.js';
 
 async function probarConexion() {
     try{
-        await Sequelize.authenticate();
+        await sequelize.authenticate();
         console.log('Conexión establecida correctamente.');
     } catch (error) {
         console.error('No se pudo conectar a la base de datos:', error.message );
