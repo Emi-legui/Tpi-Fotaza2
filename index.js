700f3c0b87477ede8df991ed99fe0dba2e56cb38
Emi-legui <emilegui76@gmail.com>
2026-06-03 15:47:38 -0300

    moficando index

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import sequelize from './config/database.js';
+
+const app = express();
+
+app.use(express.json());
+
+// Iniciamos la conexion con la base de datos
+sequelize.sync()
+    .then(() => {
+        console.log('Conexión con la base de datos exitosa.');
+        
+        const PORT = 3000;
+        app.listen(PORT, () => {
+            console.log(`Servidor corriendo en http://localhost:${PORT}`);
+        });
+    })
+    .catch(error => {
+        console.error('Error al conectar con la base de datos:', error);
+    });
