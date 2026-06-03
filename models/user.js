1704850e070bb9bc77d73d15310f07fda99746fd
Emi-legui <emilegui76@gmail.com>
2026-06-03 02:08:29 -0300

    creando carpeta config y realizando user.js (Usuario)

---
+++ models/user.js
@@ -0,0 +1,34 @@
+import {database} from 'sequelize';
+import sequelize from '../config/database.js';
+
+const User = sequelize.define('User',{
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    email:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    es_validaor: { 
+        type: DataTypes.BOOLEAN,
+        defaultValue: false 
+    },
+    esta_activo: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true 
+    }
+});
+export default User;
