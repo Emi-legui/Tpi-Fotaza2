7da8f71a5842bae0255d21c51f8eaa7ffa8d67a4
Emi-legui <emilegui76@gmail.com>
2026-06-03 16:15:37 -0300

    importando user en index

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import sequelize from './config/database.js';
+import User from './models/user.js';
 
 const app = express();
 
 app.use(express.json());
 
 // Iniciamos la conexion con la base de datos
-sequelize.sync()
+sequelize.sync({ force: false })
     .then(() => {
         console.log('Conexión con la base de datos exitosa.');
         
