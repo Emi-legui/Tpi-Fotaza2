ab1bcc9c51454f132c451601d1d614c68ccb72af
Emi-legui <emilegui76@gmail.com>
2026-06-03 17:02:59 -0300

    importamos userRoutes en index

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import sequelize from './config/database.js';
 import User from './models/user.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api/users', userRoutes);
 
 // Iniciamos la conexion con la base de datos
 sequelize.sync({ force: false })
