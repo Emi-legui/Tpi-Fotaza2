69b59c9f7e8d358495952a0b6142b7c4d0585f99
Emi-legui <emilegui76@gmail.com>
2026-06-05 22:56:45 -0300

    importando post y postRoutes en index

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import sequelize from './config/database.js';
 import User from './models/user.js';
+import Post from './models/post.js';
 import userRoutes from './routes/userRoutes.js';
+import postRoutes from './routes/postRoutes.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
 
 // Iniciamos la conexion con la base de datos
 sequelize.sync({ force: false })
