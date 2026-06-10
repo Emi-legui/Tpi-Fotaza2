6059d12d11106a995b7be6733f31761d7a15a844
Emi-legui <emilegui76@gmail.com>
2026-06-10 14:36:20 -0300

    agregando rutas get

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,9 +3,22 @@ import { login, register } from '../controllers/authController.js';
 
 const router = express.Router();
 
-// Ruta para iniciar sesion
+//rutas para mostrar los formularios (GET)
+
+//Muestra el formulario de login
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+//Muestra el formulario de registro
+router.get('/register', (req, res) => {
+    res.render('register');
+});
+
+//Rutas para porcesar los formularios (POST)
+
+// Procesa los datos del login
 router.post('/login', login);
-// Ruta para registrar un nuevo usuario 
+// Procesa los datos del registro
 router.post('/register', register);
 
 export default router;
