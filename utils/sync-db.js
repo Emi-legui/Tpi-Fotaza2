d40fea93ebd37de98392eabc2ff601934b5cb584
Emi-legui <emilegui76@gmail.com>
2026-06-10 13:25:21 -0300

    importando en sync-db

--- utils/sync-db.js
+++ utils/sync-db.js
@@ -1,12 +1,15 @@
 import sequelize from '../config/database.js';
 import User from '../models/user.js';
+import Post from '../models/post.js';
+import Comment from '../models/comment.js';
+import Denuncia from '../models/denuncia.js';
 
 async function sincronizarBaseDeDatos() {
     try {
         await sequelize.sync({ force: true });
         console.log('Tablas creadas con exito en la base de datos.');
     } catch (error) {
-        console.error('Error al sincronizar:', error.message);
+        console.error('Error al sincronizar la base de datos:', error.message);
     } finally {
         process.exit();
     }
