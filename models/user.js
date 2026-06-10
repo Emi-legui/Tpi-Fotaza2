674d2615280b61a30f1bac8f663b035c2991ef39
Emi-legui <emilegui76@gmail.com>
2026-06-10 02:25:15 -0300

    modificando user

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 import {DataTypes} from 'sequelize';
 import sequelize from '../config/database.js';
+import bcrypt from 'bcryptjs';
+import { Hooks } from 'sequelize/lib/hooks';
 
 const User = sequelize.define('User',{
     id:{
@@ -29,6 +31,21 @@ const User = sequelize.define('User',{
     esta_activo: {
         type: DataTypes.BOOLEAN,
         defaultValue: true 
+    },
+    Hooks: {
+        //antes de crear o actualizar un usuario, se encripta la contraseña
+        beforeCreate: async (user) => {
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(user.password, salt);
+        },
+        
+        beforeUpdate: async (user) => {
+            if(user.changed('password')) {
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(user.password, salt);
+            }
+        }
     }
+
 });
 export default User;
